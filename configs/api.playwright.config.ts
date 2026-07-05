1f09dd689b7da04f2e4818f43bcc167db3347efb
Anilkumar2127 <cyberattack554@gmail.com>
2026-07-05 17:10:48 +0530

    modified changes

--- configs/api.playwright.config.ts
+++ configs/api.playwright.config.ts
@@ -35,7 +35,7 @@ export default defineConfig({
   workers: Number(process.env.PARALLELMODE),
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
     reporter: [
-                ['html' , {outputFolder:path.resolve(__dirname,'../reports/finalreport')}]
+                ['html' , {outputFolder:path.resolve(__dirname,'../reports/apifinalreport')}]
               ],
   
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
